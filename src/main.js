896bb934bda34ec4fd503cc3c31a3d48fcdf1a32
lysmac <calle@lysmask.se>
2023-01-05 14:24:01 +0100

    some styling and it now places out fruits randomly in the grid

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 var fruits = ["🍍", "🍒", "🍊", "🍋", "🍓", "🍑", "🍌", "🍎"];
+// Shuffles an array
 function shuffle(arr) {
     var shuffled = arr
         .map(function (value) { return ({ value: value, sort: Math.random() }); })
@@ -9,10 +10,25 @@ function shuffle(arr) {
     });
     return shuffled;
 }
+// Merges an array two times, and shuffles it
 function mergeArrays() {
     var first = shuffle(fruits);
     var second = shuffle(fruits);
     var shuffledDoubleFruits = first.concat(second);
-    console.log(shuffledDoubleFruits);
+    var reShuffle = shuffle(shuffledDoubleFruits);
+    return reShuffle;
 }
-mergeArrays();
+function placeOutFruits() {
+    var mixedFruits = mergeArrays();
+    console.log(mixedFruits);
+    var board = document.getElementById("board");
+    mixedFruits.forEach(function (fruit) {
+        var div = document.createElement("div");
+        div.className = "card";
+        div.textContent = fruit;
+        board === null || board === void 0 ? void 0 : board.appendChild(div);
+        console.log(fruit);
+        // main.appendChild(document.createElement("div")).textContent = fruit;
+    });
+}
+placeOutFruits();
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 const fruits: Array<string> = ["🍍", "🍒", "🍊", "🍋", "🍓", "🍑", "🍌", "🍎"];
 
+// Shuffles an array
 function shuffle<String>(arr: String[]) {
   let shuffled = arr
     .map((value) => ({ value, sort: Math.random() }))
@@ -8,12 +9,32 @@ function shuffle<String>(arr: String[]) {
   return shuffled;
 }
 
+// Merges an array two times, and shuffles it
 function mergeArrays<String>() {
   const first: string[] = shuffle(fruits);
   const second: string[] = shuffle(fruits);
 
   const shuffledDoubleFruits: string[] = first.concat(second);
-  console.log(shuffledDoubleFruits);
+  const reShuffle = shuffle(shuffledDoubleFruits);
+  return reShuffle;
 }
 
-mergeArrays();
+function placeOutFruits() {
+  const mixedFruits = mergeArrays();
+
+  console.log(mixedFruits);
+
+  const board = document.getElementById("board");
+
+  mixedFruits.forEach((fruit) => {
+    const div = document.createElement("div");
+    div.className = "card";
+    div.textContent = fruit;
+    board?.appendChild(div);
+    console.log(fruit);
+
+    // main.appendChild(document.createElement("div")).textContent = fruit;
+  });
+}
+
+placeOutFruits();
